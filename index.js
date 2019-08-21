438345cae34eb3d8d991eb3d07a32a2b8700efdf
k-westbrook <kcheriewestbrook@gmail.com>
2019-08-21 15:27:45 -0700

    initial push

---
+++ index.js
@@ -0,0 +1,34 @@
+var { Client } = require('pg')
+exports.handler = async function (event) {
+  try {
+
+
+    const client = new Client({
+      host: process.env.HOST_REFERENCE,
+      user: process.env.USER_NAME,
+      password: process.env.PASSWORD_REFERENCE,
+      database: process.env.DATABASE_NAME
+    });
+    await client.connect();
+    let results = await client.query(`INSERT INTO public."Comments"(
+      restaurant_id, comment, name)
+      VALUES (${event.restaurant_id},${event.comment},${event.name} )`);
+
+
+    console.log(results)
+
+    let response =
+    {
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      },
+      status: 200,
+    };
+    return response;
+
+  } catch (err) {
+    return err;
+  }
+}
+
+
